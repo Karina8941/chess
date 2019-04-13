c517e1e15af33568e111f6017b8cad9d426dcdef
Карина Елагина <karina.elagina2013@yandex.ru>
2019-04-13 14:33:09 +0700

    Test version two

--- src/move.cpp
+++ src/move.cpp
@@ -13,7 +13,8 @@ void convert(char s[])
 void move(char* dos[])
 {
     char s[6];
-    1 : cin >> s;
+L:
+    cin >> s;
     if (s[5] != '\0')
         cout << "Error input" << endl;
     convert(s);
@@ -21,13 +22,14 @@ void move(char* dos[])
     for (int i = 0; i < 5; i++) {
         if (!(s[i] >= 49 && s[i] <= 56)) {
             cout << "Error input" << endl;
-            goto 1;
+            goto L;
         }
         if (i == 1)
             i++;
     }
 
     if (s[0] == s[3] && (s[4] - s[1] == 1 || (s[0] == 1 && s[4] - s[1] == 2)))
-        swap(dos[(char)s[1]][(char)s[0]], dos[(char)s[4]][(char)s[3]]);
+        swap(dos[(int)(char)s[1]][(int)(char)s[0]],
+             dos[(int)(char)s[4]][(int)(char)s[3]]);
 }
 }
