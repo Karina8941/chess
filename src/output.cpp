b48ef662c3fdbd2419668afbdcb2fb980c9fd83b
Карина Елагина <karina.elagina2013@yandex.ru>
2019-04-13 10:21:39 +0700

    Bag fix

--- src/output.cpp
+++ src/output.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
-for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++)
-        std::cout << dos[i][j];
-    std::cout << std::endl;
+void print(int* dos[])
+{
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++)
+            std::cout << dos[i][j];
+        std::cout << std::endl;
+    }
 }
