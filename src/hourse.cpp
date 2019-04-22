88c899431209e195821fabc1adef752eb7b33054
Карина Елагина <karina.elagina2013@yandex.ru>
2019-04-22 15:40:04 +0700

    Create house

---
+++ src/hourse.cpp
@@ -0,0 +1,10 @@
+#include "foo.h"
+#include <iostream>
+
+void hourse(char* dos[], char s[])
+{
+    if (((abs(s[0] - s[3]) == 2 && abs(s[1] - s[4]) == 1) || (abs(s[0] - s[3]) == 1 && abs(s[1] - s[4]) == 2)) && dos[(int)(char)s[4]][(int)(char)s[3]] == '0')//проверка на коррекность введённых данных и на существование пешек в этом месте
+        swap(dos[(int)(char)s[1]][(int)(char)s[0]],
+             dos[(int)(char)s[4]][(int)(char)s[3]]);
+
+}
