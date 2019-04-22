770c3fc0744b9a62835cd5c4df77983cb389572b
Карина Елагина <karina.elagina2013@yandex.ru>
2019-04-22 14:28:22 +0700

    Realise move elephant

--- src/move.cpp
+++ src/move.cpp
@@ -1,8 +1,6 @@
 #include "foo.h"
 #include <iostream>
 
-using namespace std;
-
 void move(char* dos[], char s[])
 {
     if (s[0] == s[3] && (s[4] - s[1] == 1 || (s[0] == 1 && s[4] - s[1] == 2) && dos[(int)(char)s[4]][(int)(char)s[3]] == '0') //проверка на коррекность введённых данных и на существование пешек в этом месте
--- src/rook.cpp
+++ src/rook.cpp
@@ -1,8 +1,6 @@
 #include "foo.h"
 #include <iostream>
 
-using namespace std;
-
 void rook(char* dos[], char s[])
 {
     if ((s[0] == s[3] || s[1] == s[4]) && dos[(int)(char)s[4]][(int)(char)s[3]] == '0')//проверка на коррекность введённых данных и на существование пешек в этом месте
